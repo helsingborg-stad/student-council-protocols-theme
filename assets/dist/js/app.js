fcf1a9cae093d0d78a8789498e09e3d08a5f5380
Max Frederiksen <max@colesmedia.se>
2020-01-10 11:16:31 +0100

    first work on the front page

--- assets/dist/js/app.js
+++
@@ -1,3 +0,0 @@
-var Elevroden;
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBFbGV2cm9kZW47XG4iXX0=
--- templates/module/items.blade.php
+++ templates/module/items.blade.php
@@ -1,80 +1,73 @@
 @include('partials.post-filters')
 
-<div class="grid" data-equal-container>
-    @if (!$hideTitle && !empty($post_title))
-    <div class="grid-xs-12">
-        <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+@if (!$hideTitle && !empty($post_title))
+<h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+@endif
+<section class="protocols start-page-protocols">
+    <div class="container">
+        <div class="grid grid--columns" data-equal-container>
+            <div class="start-page-ctas">
+                    <a href="{{ home_url() }}/protocol" class="btn-oval btn-full">
+                        <span><?php _e('View all posts', 'elevroden'); ?></span>
+                    </a>
+                    @if (is_user_logged_in())
+                        <a href="{{ home_url() }}/create-protocol" class="btn-oval btn-full">
+                            <i class="pricon pricon-plus"></i>
+                            <span><?php _e('Write a new post', 'elevroden'); ?></span>
+                        </a>
+                    @endif
+            </div>
+
+            @if (count($posts) > 0)
+                @foreach ($posts as $post)
+                    {{-- <div class="{{ $posts_columns }}"> --}}
+                    <div class="u-flex grid-xs-12 grid-sm-12 grid-md-4 grid-lg-3">
+                        <a href="{{ $posts_data_source === 'input' ? $post->permalink : get_permalink($post->ID) }}" class="card">
+                            <div class="card-body">
+                                @if (in_array('title', $posts_fields))
+                                <h5 class="card-title">{!! apply_filters('the_title', $post->post_title) !!}</h5>
+                                @endif
+
+                                
+
+                                @if (get_current_user_id() == get_post_field('post_author', $post->ID))
+                                    <object>
+                                        <a href="{{ $posts_data_source === 'input' ? $post->permalink . '#modal-edit-post' : get_permalink($post->ID) . '#modal-edit-post' }}" class="card-edit-btn">
+                                            <i class="pricon pricon-pen pricon-pen-white"></i>
+                                        </a>
+                                    </object>
+                                @endif
+
+                                <div class="card-info">
+                                    <span class="card-small-text">{{ get_post_meta($post->ID)["name_of_council_or_politician"][0] }}</span>
+                                    <br>
+                                    @if (in_array('date', $posts_fields) && $posts_data_source !== 'input')
+                                        <time class="card-small-text">{{ apply_filters('Modularity/Module/Posts/Date', get_the_time('Y-m-d', $post->ID), $post->ID, $post->post_type)  }}</time>
+                                    @endif
+                                    
+                                    <object><a href="{{ $posts_data_source === 'input' ? $post->permalink . '#comments': get_permalink($post->ID) . '#comments'}}" class="card-small-text card-comments-link"> Comments ({{ apply_filters('Modularity/Module/Posts/commentCount', $post->ID) }})</a></object>
+                                    
+                                </div>
+
+                                @if (in_array('excerpt', $posts_fields))
+                                    @if ($posts_data_source === 'input')
+                                        {!! $post->post_content !!}
+                                    @else
+                                        <p>{!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(get_extended($post->post_content)['main']), 30, null)) : '' !!}</p>
+                                    @endif
+                                @endif
+
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            @else
+
+                <div class="grid-md-12">
+                    <?php _e('No posts to show…', 'modularity'); ?>
+                </div>
+
+            @endif
+        </div>
     </div>
-    @endif
-
-    @if (count($posts) > 0)
-      @foreach ($posts as $post)
-        <?php 
-          $postMeta = get_post_meta($post->ID);
-          $targetGroup = $postMeta['target_group'][0]
-        ?>
-        @if($targetGroup == 'council')
-          <div class="{{ $posts_columns }}">
-              <a href="{{ $posts_data_source === 'input' ? $post->permalink : get_permalink($post->ID) }}" class="{{ $classes }}" data-equal-item>
-                  @if ($post->thumbnail && in_array('image', $posts_fields))
-                      <div class="box-image-container">
-                          <?php if (isset($taxonomyDisplay['top'])) : foreach ($taxonomyDisplay['top'] as $taxonomy => $placement) : $terms = wp_get_post_terms($post->ID, $taxonomy); if (count($terms) > 0) : ?>
-                              <ul class="tags-<?php echo $taxonomy; ?> pos-absolute-<?php echo $placement; ?>">
-                                  <?php foreach ($terms as $term) : ?>
-                                      <li class="tag tag-<?php echo $term->taxonomy; ?> tag-<?php echo $term->slug; ?>"><?php echo $term->name; ?></li>
-                                  <?php endforeach; ?>
-                              </ul>
-                          <?php endif; endforeach; endif; ?>
-
-                          <img src="{{ $post->thumbnail[0] }}" alt="{{ $post->post_title }}" class="box-image">
-                      </div>
-                  @endif
-
-                  <div class="box-content">
-                      
-                      @if (in_array('title', $posts_fields))
-                      <h5 class="link-item link-item-light">{!! apply_filters('the_title', $post->post_title) !!}</h5>
-                      @endif
-
-                      <a class="link-item" href="{{ get_permalink($post->ID) . '#modal-edit-post' }}">EDIT POST</a>
-
-                      <a class="link-item" href="{{ get_permalink($post->ID) . '#comments' }}">COMMENTS <span>{{count(get_comments(array('post_id' => $post->ID)))}}</span></a>
-
-                      @if (in_array('date', $posts_fields) && $posts_data_source !== 'input')
-                      <p><time>{{ apply_filters('Modularity/Module/Posts/Date', get_the_time('Y-m-d H:i', $post->ID), $post->ID, $post->post_type)  }}</time></p>
-                      @endif
-
-                      @if (in_array('excerpt', $posts_fields))
-                          @if ($posts_data_source === 'input')
-                              {!! $post->post_content !!}
-                          @else
-                              <p>{!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(get_extended($post->post_content)['main']), 30, null)) : '' !!}</p>
-                          @endif
-                      @endif
-
-                      <?php if (isset($taxonomyDisplay['below'])) : foreach ($taxonomyDisplay['below'] as $taxonomy => $placement) : $terms = wp_get_post_terms($post->ID, $taxonomy); if (count($terms) > 0) : ?>
-                          <ul class="tags tags-<?php echo $taxonomy; ?>">
-                              <?php foreach ($terms as $term) : ?>
-                                  <li class="tag tag-<?php echo $term->taxonomy; ?> tag-<?php echo $term->slug; ?>"><?php echo $term->name; ?></li>
-                              <?php endforeach; ?>
-                          </ul>
-                      <?php endif; endforeach; endif; ?>
-                  </div>
-              </a>
-          </div>
-        @endif
-      @endforeach
-      @else
-
-      <div class="grid-md-12">
-          <?php _e('No posts to show…', 'modularity'); ?>
-      </div>
-
-      @endif
-
-      @if ($posts_data_source !== 'input' && isset($archive_link) && $archive_link && $archive_link_url)
-      <div class="grid-lg-12">
-          <a class="read-more" href="{{ $archive_link_url }}?{{ http_build_query($filters) }}"><?php _e('Show more', 'modularity'); ?></a>
-      </div>
-      @endif
-</div>
+</section>
