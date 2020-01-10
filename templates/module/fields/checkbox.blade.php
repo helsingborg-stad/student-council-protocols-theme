ef12ee3783ac8e026bde89950b9d1486a4de3240
Max Frederiksen <max@colesmedia.se>
2020-01-10 10:55:39 +0100

    fixed modularity form builder checkbox for validation

--- templates/module/fields/checkbox.blade.php
+++ templates/module/fields/checkbox.blade.php
@@ -1,11 +1,11 @@
 <div class="grid mod-form-field" {!! $field['conditional_hidden'] !!}>
     <div class="grid-md-12">
-        <div class="form-group">
+        <div class="form-group checkbox-group">
             <label for="{{ $module_id }}-{{ sanitize_title($field['label']) }}">{{ $field['label'] }}{!!  $field['required'] ? '<span class="text-danger">*</span>' : '' !!}</label>
             {!! !empty($field['description']) ? '<div class="text-sm text-dark-gray">' . ModularityFormBuilder\Helper\SanitizeData::convertLinks($field['description']) . '</div>' : '' !!}
             @foreach ($field['values'] as $value)
-                <label class="checkbox-container">
-                    <input class="checkbox" type="checkbox" name="{{ sanitize_title($field['label']) }}[]" value="{{ $value['value'] }}" {{ $field['required'] ? 'required' : '' }}>
+                <label class="checkbox checkbox-container">
+                    <input class="input-checkbox {{ $field['required'] ? 'required' : '' }}" type="checkbox" name="{{ sanitize_title($field['label']) }}[]" value="{{ $value['value'] }}">
                     <span class="checkmark"></span>
                     <span class="checkbox-title">{{ $value['value'] }}</span>
                 </label>
