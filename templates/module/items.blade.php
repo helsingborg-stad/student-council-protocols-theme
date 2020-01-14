0b0f53cc9595c3027ccceef063b9aebdda0c6fc3
Max Frederiksen <max@colesmedia.se>
2020-01-14 08:56:14 +0100

    renaming and final changes to front page layout

--- templates/module/items.blade.php
+++ templates/module/items.blade.php
@@ -1,9 +1,9 @@
 @include('partials.post-filters')
 
 @if (!$hideTitle && !empty($post_title))
-<h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+<h4 class="start-page-title">{!! apply_filters('the_title', $post_title) !!}</h4>
 @endif
-<section class="protocols start-page-protocols">
+<section class="start-page-section">
     <div class="container">
         <div class="grid grid--columns" data-equal-container>
             <div class="start-page-ctas">
@@ -21,14 +21,12 @@
             @if (count($posts) > 0)
                 @foreach ($posts as $post)
                     {{-- <div class="{{ $posts_columns }}"> --}}
-                    <div class="u-flex grid-xs-12 grid-sm-12 grid-md-4 grid-lg-3">
+                    <div class="u-flex grid-xs-12 grid-sm-6 grid-md-4 grid-lg-3">
                         <a href="{{ $posts_data_source === 'input' ? $post->permalink : get_permalink($post->ID) }}" class="card">
                             <div class="card-body">
                                 @if (in_array('title', $posts_fields))
                                 <h5 class="card-title">{!! apply_filters('the_title', $post->post_title) !!}</h5>
                                 @endif
-
-                                
 
                                 @if (get_current_user_id() == get_post_field('post_author', $post->ID))
                                     <object>
@@ -53,7 +51,7 @@
                                     @if ($posts_data_source === 'input')
                                         {!! $post->post_content !!}
                                     @else
-                                        <p>{!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', wp_trim_words(wp_strip_all_tags(get_extended($post->post_content)['main']), 30, null)) : '' !!}</p>
+                                        <p>{!! isset(get_extended($post->post_content)['main']) ? apply_filters('the_excerpt', substr(wp_trim_words(wp_strip_all_tags(get_extended($post->post_content)['main']), 30, null), 0, 100) . '...') : '' !!}</p>
                                     @endif
                                 @endif
 
