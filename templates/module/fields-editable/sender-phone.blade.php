17f0314b87dc7fd49ff1da6eee2b73781d75a746
Max Frederiksen <max@colesmedia.se>
2020-01-30 10:46:00 +0100

    removed unsused files

--- templates/module/fields-editable/sender-phone.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- Phone --}}
-<div class="mod-form-field">
-    <p><b><label for="{{ $module_id }}-phone">
-        {{ $field['label'] }}{!! $field['required'] ? '<span class="text-danger">*</span>' : '' !!}
-    </label></b></p>
-    <input type="tel" name="mod-form[{{ $field['name'] }}]" class="large-text" id="{{ $module_id }}-phone" value="{{ $field['value'] }}" {{ $field['required'] ? 'required' : '' }}>
-</div>
